f3118c2a45f19d41a3721a95d6b3fe871afd3709
Abdurrahman404 <bmabdurrahman5@gmail.com>
2026-08-20 14:20:56 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+
+
+ 
+
+
+const app = express();
+const port = process.env.PORT ;
+
+
+
+app.listen(port, () => {
+    console.log("Server is running on port 3000");
+})
